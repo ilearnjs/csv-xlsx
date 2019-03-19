690d844effe881e73dbe1232dfca3b169481514d
ilearnjs <antonenko.pvl@gmail.com>
2019-03-19 14:03:20 +0300

    init commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var xlsx = require('xlsx')
+
+var inputFileCsv = './app/files/layout.csv';
+var inputFileXlsx = './app/files/layout.csv';
+
+var workbook = xlsx.readFile(inputFileXlsx)
+var first_sheet_name = workbook.SheetNames[0];
+var worksheet = workbook.Sheets[first_sheet_name];
+var csv1 = xlsx.utils.sheet_to_csv(worksheet);
+
+var csv2 = fs.readFileSync(inputFileCsv).toString();
+
+console.log(csv1)
+console.log(csv2)
